b2e00e6998bb39f384b21fd252eef86b0e636aee
Ann-Mariya-shaju <118293630+Ann-Mariya-shaju@users.noreply.github.com>
2024-12-12 10:56:36 +0530

    add js

---
+++ program2.js
@@ -0,0 +1,68 @@
+// datatypes
+
+console.log(typeof("hi"));
+console.log(typeof(true));
+console.log(typeof(114));
+var firstName="Ann";
+console.log("--------");
+console.log(firstName);
+
+var lastName;
+console.log("----------------");
+console.log(typeof(lastName));
+
+
+// operator
+// Arithmetic Operator
+console.log(5+4)
+console.log(5%2)
+console.log(5*2)
+
+// comparison operator
+
+var num1 =2;
+var num2 = 2;
+console.log(num1==num2);
+
+var num1 =2;
+var name = 'ann';
+console.log(num1=== num2);
+console.log("------------");
+
+var num1 =2;
+var num2 = 4;
+console.log(num1 != num2);
+console.log("------------");
+
+
+// increment and decrement
+var mark = 30;
+console.log(++mark);
+console.log(--mark);
+console.log("------------");
+
+// concatention
+var x=4;
+var y =5;
+console.log(x+y);
+console.log("------------");
+
+var x=4;
+var y ="5";
+console.log(x+y);
+console.log("------------");
+
+var x="Hi";
+var y ="Hello";
+console.log(x+y);
+
+// Teamplate literal
+
+var companyName="TCS";
+console.log("I am working in",companyName);
+
+// or used like this
+
+var name="ann";
+var companyName="IBM";
+console.log(`my name is ${name} and I am working in ${companyName}`);

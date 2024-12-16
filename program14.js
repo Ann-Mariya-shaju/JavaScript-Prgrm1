39fb8b6c307200f25f12a8644fbb5b5f95411e4d
Ann-Mariya-shaju <118293630+Ann-Mariya-shaju@users.noreply.github.com>
2024-12-16 11:04:59 +0530

    js code

---
+++ program14.js
@@ -0,0 +1,15 @@
+// 1
+// 1 2
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5
+
+for(i=1; i<=5; i++){
+    //i<=5 ennu parayunnadhu ee patternil ethra line undo adhanu
+    var str="";
+    for(j=1;j<=i; j++){
+        str = str+j+' '
+    }
+    console.log(str);
+    
+}

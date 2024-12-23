b50ebe2f80e99687f18f6095b6c58c8285fbc9cc
Ann-Mariya-shaju <118293630+Ann-Mariya-shaju@users.noreply.github.com>
2024-12-23 12:15:29 +0530

    array

---
+++ Array/program1.js
@@ -0,0 +1,26 @@
+// Array
+var names = ['Ann','Sharon','Shaju','Viji','Jo','Athira'];
+// length
+console.log(names.length);  //output is like No.of elements in in array =6
+// indexis used to access an element in an array
+
+//names[0]  //Ann
+//names[3] // Viji
+
+
+//push
+names.push('anjana')
+console.log(names);
+
+// pop
+names.pop()
+console.log(names);
+
+// shift
+names.shift()
+console.log(names);
+
+// unshift
+names.unshift('Malavikha')
+console.log(names);
+
